a0798e9dcc2b0141cf9c655e8d2da17ec1fab4e8
perezalvareza271@gmail.com <perezalvareza271@gmail.com>
2018-03-22 17:52:18 -0600

    nueva consulta

--- consultas2.sql
+++ consultas2.sql
@@ -1,28 +1,9 @@
-use cineme
+use cineme;
 
 
 
-select 
-h.sucursal_id,
-s.direccion,
-h.pelicula_id,
-p.nombre,
-h.dia,
-h.fecha,
-h.hora_inicio,
-h.hora_fin
-from
-    horarios h
-     left join peliculas p on p.id=h.pelicula_id left join sucursales s on  s.id=h.sucursal_id
-     where p.id=1  && s.id=1
-     
-     select 
-v.precio_boleto,
-h.dia,
-s.direccion,
-p.nombre,
-h.hora_inicio,
-h.hora_fin
-from 
-   ventas v left join horarios h on h.id=v.id left join sucursales s on s.id=h.id left join peliculas p on p.id=h.id
+ select v.precio_boleto as ventas ,h.dia ,s.direccion,p.nombre from ventas v 
+ left join horarios  h on  v.id=.id
+ left join sucursales s  on s.id=h.sucursal_id
+ left join peliculas p on  p.id=h.pelicula_id where p.id=1;
      
