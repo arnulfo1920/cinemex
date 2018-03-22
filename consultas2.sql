6bd724844b8eb07842396c790ab31a35b52eeebe
perezalvareza271@gmail.com <perezalvareza271@gmail.com>
2018-03-22 12:12:47 -0600

    nuevo commit de consulta

--- consultas2.sql
+++ consultas2.sql
@@ -14,9 +14,15 @@ h.hora_fin
 from
     horarios h
      left join peliculas p on p.id=h.pelicula_id left join sucursales s on  s.id=h.sucursal_id
-     where p.id=1 
+     where p.id=1  && s.id=1
      
-     
-     
-     && s.id=1
+     select 
+v.precio_boleto,
+h.dia,
+s.direccion,
+p.nombre,
+h.hora_inicio,
+h.hora_fin
+from 
+   ventas v left join horarios h on h.id=v.id left join sucursales s on s.id=h.id left join peliculas p on p.id=h.id
      
